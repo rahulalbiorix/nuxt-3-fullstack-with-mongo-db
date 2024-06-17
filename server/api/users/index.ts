e0564ac557afb35a92e9fbd9c92616ff7649071d
rahulalbiorix <rahulg.albiorix@gmail.com>
2024-06-17 17:48:23 +0530

    remove user api

--- server/api/users/index.ts
+++
@@ -1,8 +0,0 @@
-import UserModel from "~/server/models/User.model";
-
-export default defineEventHandler(async (event) => {
-  const users = await UserModel.find();
-  return {
-    users,
-  };
-});
